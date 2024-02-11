925106fa2ee6fdea74730a540b393377ecc2e5eb
Manikandan Radha <manikandanradha15@gmail.com>
2024-02-11 12:33:37 +0530

    added variable.py

---
+++ python/01-basics/variable.py
@@ -0,0 +1,29 @@
+number1 = 5
+number2 = 3
+print(number1 + number2)
+
+
+sum = number1 + number2
+print(sum)
+
+
+sub = number1 - number2
+print(sub)
+
+name = "manikandan"
+
+print(name)
+print("my name is mani")
+
+fatherName = "radha"
+print(name + fatherName)
+
+
+fullName = name + fatherName
+print(fullName)
+
+
+print(f"addition = {sum} subraction = {sub} name =  {fullName}")
+
+
+
