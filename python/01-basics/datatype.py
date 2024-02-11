aa6edc8580e9d5fbf18cbac84cd08bccf3bd9df3
Manikandan Radha <manikandanradha15@gmail.com>
2024-02-11 13:33:08 +0530

    added datatype.py

---
+++ python/01-basics/datatype.py
@@ -0,0 +1,34 @@
+number1 = 5
+print(type(number1))
+
+name = "mani"
+print(type(name))
+
+
+weight = 54.5
+print(type(weight))
+
+section = 12j
+print(type(section))
+
+
+fruits = ["apple","mango","banana"]
+print(type(fruits))
+
+
+vegetables = ("betroot","gova","ladysfinger")
+print(type(vegetables))
+
+age = range(17)
+print(type(age))
+
+nameAge = {"name" : "mani" , "age" : "17"}
+print(type(nameAge))
+
+
+animal = {"lion" , "bear" , "tiger"}
+print(type(animal))
+
+
+
+
