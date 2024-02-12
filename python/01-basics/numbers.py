888f685b379981522adaeb716090231785cb0bb9
Manikandan Radha <manikandanradha15@gmail.com>
2024-02-12 19:05:47 +0530

    added new string

--- python/01-basics/numbers.py
+++ python/01-basics/numbers.py
@@ -8,3 +8,58 @@ print(type(weight))
 print(type(group))
 
 
+
+
+numbers = 12345678910
+negative = -112345
+
+
+print(type(numbers))
+print(type(negative))
+
+
+
+
+decimal = 0.76666
+nDecimal = -9.156
+
+print(type(decimal))
+print(type(nDecimal))
+
+
+
+positivefloat = 467j
+negativefloat = -78990j
+
+print(type(positivefloat))
+print(type(negativefloat))
+
+
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+import random
+
+print(random.randrange(1 , 50))
+
+
---
+++ python/01-basics/string.py
@@ -0,0 +1,27 @@
+
+
+myName = 'manikandan'
+brother = 'kumaran'
+sister = 'sindhu'
+
+print(myName)
+print(brother)
+print(sister)
+
+note = 'i am the beginer of python programing'
+print(note)
+
+
+
+
+name = "manikandan"
+print(name[8])
+
+
+
+
+for name in "manikandan" :
+    print(name)
+
+
+
