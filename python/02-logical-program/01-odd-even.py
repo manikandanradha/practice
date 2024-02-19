365af66b16be8399b375ad6be336883ac56be1c2
Manikandan Radha <manikandanradha15@gmail.com>
2024-02-19 14:31:14 +0530

    added new file

---
+++ python/02-logical-program/01-odd-even.py
@@ -0,0 +1,12 @@
+
+num = 8
+calculation = num % 2
+
+
+if (calculation == 0):
+    print("the number is even")
+else :
+    print("the number is odd")
+
+
+
---
+++ python/02-logical-program/02-positive-negative.py
@@ -0,0 +1,10 @@
+
+
+num = 0
+if num < 0 :
+    print("the num is negative")
+elif num > 0 :
+    print("the num is positive")
+else :
+    print("the num is 0")
+
