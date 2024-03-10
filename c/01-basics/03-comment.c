84d204aac8a007a00f395aec1cad498033190b98
Raja Manikandan <manikandanradha15@gmail.com>
2024-03-10 17:10:38 +0530

    added comment,c

---
+++ c/01-basics/03-comment.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  // This is a comment
+  /* The code below will print the words Hello World!
+  to the screen, and it is amazing */
+  printf("helloworld");
+  return 0;
+}
