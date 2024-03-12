71879decc0075510ace3c4dd1b656ac10c37e083
Raja Manikandan <manikandanradha15@gmail.com>
2024-03-12 18:08:59 +0530

    added new 05-change-values.c

---
+++ c/01-basics/05-change-values.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+  float hamamSoapPrice = 10.5;
+  printf("hamamSoapPrice %f \n", hamamSoapPrice );
+
+  hamamSoapPrice = 15;
+  printf("hamamSoapPrice %f", hamamSoapPrice);
+
+  return 0;
+}
