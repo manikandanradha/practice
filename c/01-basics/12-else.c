8c396ab9f0704dcffd9773e5b9f8e8a0fa38ce9e
Raja Manikandan <manikandanradha15@gmail.com>
2024-10-20 18:51:15 +0530

    added hello world

--- c/01-basics/12-else.c
+++ c/01-basics/12-else.c
@@ -4,12 +4,13 @@ int main() {
   int myNum = 10;
   
   if (myNum > 0) {
-    printf("The value is a positive number.");
-  } else if (myNum < 0) {
-    printf("The value is a negative number.");
+    printf("The value is a positive number.\n");
   } else {
-    printf("The value is 0.");
+    printf("The value is 0./n");
   }
   
+  (myNum > 0) ? printf("The value is a positive number./n") : printf("The value is 0./n");
   return 0;
 }
+
+
