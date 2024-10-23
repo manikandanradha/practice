2a362de69d221bcd3af9d18bfe3854d657cd2ca4
Raja Manikandan <manikandanradha15@gmail.com>
2024-10-23 21:51:57 +0530

    changed

--- java/basics/02-scanner.java
+++ java/basics/02-scanner.java
@@ -6,7 +6,10 @@ class java{
     Scanner scan = new Scanner (System.in);
     String name = scan.nextLine();
     int age = scan.nextInt();
+    scan.nextLine();
+    String address = scan.nextLine();
     System.out.println("My Name Is "+name);
-    System.out.print("My Age Is "+age);
+    System.out.println("My Age Is "+age);
+    System.out.print("My Address Is "+address);
   }
 }
